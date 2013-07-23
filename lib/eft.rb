7cc2b5d1ad59f02a784ac79f51022bbadc0ce46f
Felix C. Stegerman <flx@obfusk.net>
2013-07-23 17:37:26 +0200

    ...

--- lib/eft.rb
+++ lib/eft.rb
@@ -9,6 +9,8 @@
 #
 # --                                                            ; }}}1
 
+require 'tempfile'
+
 require 'obfusk/util/run'
 require 'obfusk/util/term'
 
@@ -19,11 +21,8 @@ module Eft
   # --scrolltext
 
   # --yesno text height width
-  # --msgbox text height width
-  # --infobox text height width ?
   # --inputbox text height width [init] > STDERR
   # --passwordbox text height width
-  # --textbox file height width
   # --menu text height width menu-height [ tag item ] ...
   # --checklist text height width list-height [ tag item status ] ...
   # --radiolist text height width list-height [ tag item status ] ...
@@ -47,7 +46,20 @@ module Eft
 
   WHIPTAIL = 'whiptail'
 
-  EXIT = { 0 => [:ok, :yes], 1 => [:cancel, :no], 255 => [:esc] }
+  EXIT = { ok_yes: 0, cancel_no: 1, esc: 255 }
+
+  WHAT = {                                                      # {{{1
+    show_info:  '--infobox',
+    show_msg:   '--msgbox',
+    show_text:  '--textbox',
+    ask:        '--inputbox',
+    ask_pass:   '--passwordbox',
+    ask_yesno:  '--yesno',
+    check:      '--checklist',
+    menu:       '--menu',
+    radio:      '--radiolist',
+    gauge:      '--gauge',
+  }                                                             # }}}1
 
   OPTS = {                                                      # {{{1
     all: {
@@ -73,12 +85,19 @@ module Eft
     },
   }                                                             # }}}1
 
+  OPT = ->(o,x) { o.select { |k,v| OPTS[x][k] }
+                      .map { |k,v| OPTS[x][k][v] } }
+
   # --
 
+  class Error < RuntimeError; end
+
   class Cfg
-    attr_reader :cfg
     def initialize(cfg = {})
       @cfg = cfg
+    end
+    def config
+      @cfg.freeze
     end
   end
 
@@ -117,90 +136,157 @@ module Eft
   # --
 
   class CfgShowInfo < Cfg
-
   end
 
   class CfgShowMsg < Cfg
     include CfgEsc, CfgOK
+    def config
+      c = super
+      d = c.merge on_ok: ->(e) { c[:on_ok][] if c[:on_ok] }
+      d.freeze
+    end
   end
 
   class CfgShowText < Cfg
     include CfgEsc, CfgOK
+    def config
+      c = super
+      d = c.merge on_ok: ->(e) { c[:on_ok][] if c[:on_ok] }
+      d.freeze
+    end
   end
 
   class CfgAsk < Cfg
     include CfgEsc, CfgOK, CfgCancel
+    def config
+      c = super
+      d = c.merge on_ok: ->(e) { c[:on_ok][] if c[:on_ok] }
+      d.freeze
+    end
   end
 
   class CfgAskPass < Cfg
     include CfgEsc, CfgOK, CfgCancel
+    def config
+      c = super
+      d = c.merge on_ok: ->(e) { c[:on_ok][e.first] if c[:on_ok] }
+      d.freeze
+    end
   end
 
   class CfgAskYesNo < Cfg
     include CfgEsc, CfgYes, CfgNo
+    # ...
   end
 
   class CfgCheck < Cfg
     include CfgEsc, CfgOK, CfgCancel
+    # ...
   end
 
   class CfgMenu < Cfg
     include CfgEsc, CfgOK, CfgCancel
+    # ...
   end
 
   class CfgRadio < Cfg
     include CfgEsc, CfgOK, CfgCancel
+    # ...
   end
 
   class CfgGauge < Cfg
+    # ...
   end
 
   # --
 
-  # show message w/o OK button and clearing screen
-  def self.show_info(msg, opts = {}, &b)
+  # show message w/o OK button, don't clear screen
+  def self.show_info(text, opts = {})
+    _whip :show_info, text, CfgShowInfo, opts, [], nil
   end
 
   # show message w/ OK button
-  def self.show_msg(mgs, opts = {}, &b)
+  def self.show_msg(text, opts = {}, &b)
+    _whip :show_msg, text, CfgShowMsg, opts, [], b
   end
 
-  # file !!!
-  def self.show_text(opts = {}, &b)
+  # show file contents or text
+  def self.show_text(opts = {}, &b)                             # {{{1
+    f = ->(x) { _whip :show_text, x, CfgShowText, opts, [], b }
+    file = opts[:file]; text = opts[:text]
+    raise Error, 'can\'t have file and text' if file && text
+    raise Error, 'must have file or text' if !file && !text
+    if file
+      f[file]
+    else
+      Tempfile.open('eft') { |x| x.write text; x.close; f[x.path] }
+    end
+  end                                                           # }}}1
+
+  # --
+
+  # ask for line of input
+  def self.ask(text, opts = {}, &b)
+    _whip :ask, text, CfgAsk, opts, [], b
+  end
+
+  # ask for password
+  def self.ask_pass(text, opts = {}, &b)
+  end
+
+  # choose yes or no
+  def self.ask_yesno(text, opts = {}, &b)
   end
 
   # --
 
-  def self.ask(opts = {}, &b)
-  end
-
-  def self.ask_pass(opts = {}, &b)
-  end
-
-  def self.ask_yesno(opts = {}, &b)
-  end
-
-  # --
-
+  # choose checkboxes
   def self.check(opts = {}, &b)
   end
 
+  # choose from menu
   def self.menu(opts = {}, &b)
   end
 
+  # choose radiobox
   def self.radio(opts = {}, &b)
   end
 
   # --
 
+  # show gauge
   def self.gauge(opts = {}, &b)
   end
 
   # --
 
-  def self._whip(what, cfg, opts, args)
-
-  end
+  # process options, run whiptail, call on_{ok,yes,cancel,no,esc},
+  # return { exit:, err: }
+  # @raise Error if unknown exitstatus
+  def self._whip(what, text, cfg, opts, args, b)                # {{{1
+    x = cfg.new; b[x] if b; c = x.config
+    o = [                               OPT[opts,:all],
+      cfg.method_defined?(:on_ok)     ? OPT[opts,:ok]     : [],
+      cfg.method_defined?(:on_cancel) ? OPT[opts,:cancel] : [],
+      cfg.method_defined?(:on_yes)    ? OPT[opts,:yes]    : [],
+      cfg.method_defined?(:on_no)     ? OPT[opts,:no]     : [],
+      what == :menu                   ? OPT[opts,:menu]   : [],
+    ] .flatten
+    r = _run_whip text, c, [WHAT[what]] + o, args
+    case r[:exit]
+    when EXIT[:ok_yes]
+      c[:on_ok][r[:err]] if c[:on_ok]
+      c[:on_yes][r[:err]] if c[:on_yes]
+    when EXIT[:cancel_no]
+      c[:on_cancel][] if c[:on_cancel]
+      c[:on_no][] if c[:on_no]
+    when EXIT[:esc]
+      c[:on_esc][] if c[:on_esc]
+    else
+      raise Error, 'unknown exitstatus'
+    end
+    r
+  end                                                           # }}}1
 
   # run whiptail; return { exit: exitstatus, err: chomped_lines }
   def self._run_whip(text, cfg, opts, args)                     # {{{1
